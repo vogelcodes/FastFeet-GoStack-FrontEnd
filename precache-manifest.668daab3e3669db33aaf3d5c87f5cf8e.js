41798ef3074025340fd47b1b192f323d444afe1e
vogelcodes <54858106+vogelcodes@users.noreply.github.com>
2020-04-19 21:35:06 -0300

    Entrega 19-04-2020

---
+++ precache-manifest.668daab3e3669db33aaf3d5c87f5cf8e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eee9e8e76fd105c8ad80f7d7b6a13871",
+    "url": "/index.html"
+  },
+  {
+    "revision": "02881f9c955ec52e7843",
+    "url": "/static/css/2.f42e59c9.chunk.css"
+  },
+  {
+    "revision": "02881f9c955ec52e7843",
+    "url": "/static/js/2.ef8acddb.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.ef8acddb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5edb60d167a06452d163",
+    "url": "/static/js/main.b7d4854a.chunk.js"
+  },
+  {
+    "revision": "055e94f6602794288cc3",
+    "url": "/static/js/runtime-main.1f1f19a3.js"
+  },
+  {
+    "revision": "63764bb89d06fce82f9d15f548dc3531",
+    "url": "/static/media/fastfeet.63764bb8.svg"
+  }
+]);
